9b9cf390f1fd018eb430077a9a87f86989bd742f
rheera <rheera2@uwo.ca>
2023-11-11 17:14:11 -0500

    add add expense and load raw data buttons

--- app/routes/_auth.expenses.tsx
+++ app/routes/_auth.expenses.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "@remix-run/react";
+import { Link, Outlet } from "@remix-run/react";
 import ExpensesList from "~/components/expenses/ExpensesList";
 
 const DUMMY_EXPENSES = [
@@ -21,6 +21,38 @@ export default function Expenses() {
     <>
       <Outlet />
       <main>
+        <section id="expenses-actions">
+          <Link to="add">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <span>Add Expense</span>
+          </Link>
+          <Link to="raw" reloadDocument>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M12 2.25a.75.75 0 01.75.75v11.69l3.22-3.22a.75.75 0 111.06 1.06l-4.5 4.5a.75.75 0 01-1.06 0l-4.5-4.5a.75.75 0 111.06-1.06l3.22 3.22V3a.75.75 0 01.75-.75zm-9 13.5a.75.75 0 01.75.75v2.25a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5V16.5a.75.75 0 011.5 0v2.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V16.5a.75.75 0 01.75-.75z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <span>Load raw data</span>
+          </Link>
+        </section>
         <ExpensesList expenses={DUMMY_EXPENSES} />
       </main>
     </>
